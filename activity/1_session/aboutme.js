5fda5351620a94a14aee94d30be5c2c8dc7417c5
Katherine Vera <katherine.vera94@gmail.com>
2023-05-14 21:48:49 -0400

    soluciones actividad 1

--- activity/1_session/aboutme.js
+++ activity/1_session/aboutme.js
@@ -1,4 +1,13 @@
 console.log("hello world");
 
+let nombreApellido = "Katherine Vera";
+let profesion = "Ingeniera en Informática";
+let puesto = "Desarrolladora de Software";
+
+let presentacion = `“Hola, me llamo ${nombreApellido} y me dedico a ${profesion}.
+Estoy cursando este Máster porque me gustaría trabajar en ${puesto}.`
+
+console.log(presentacion);
+alert(presentacion);
 
 console.log("laater");
--- activity/1_session/index.js
+++ activity/1_session/index.js
@@ -7,8 +7,28 @@ console.log("Ejercicio 1");
 // Ejercicio 3
 
 // Ejercicio 4
+let divisiblesBy7 = [];
+for (let i=1; i<=100; i++){
+    if(i%7 == 0) 
+        divisiblesBy7.push(i);
+}
+
+console.log("Numeros divisibles por 7: " + divisiblesBy7.toString());
 
 // Ejercicio 5
 
 // Ejercicio 6
+let formatter = {
+    prefix: "Hello ", 
+    append: function(word){
+        console.log(this.prefix + word);
+    }
+};
+
+formatter.append("World"); // Result: Hello World
+
+formatter.toLowerString = function(word){
+    console.log(word.toLocaleLowerCase());
+}
+formatter.toLowerString("I'm Lucas"); // Result: i'm lucas
 
